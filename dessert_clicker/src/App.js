577909903d55f402d7d3e66c0a2a80ba0d9950b9
cnachurs <cnachurs@villanova.edu>
2023-12-06 16:26:23 -0500

    Added Grandma upgrade and made some changes to how upgrades work

--- dessert_clicker/src/App.js
+++ dessert_clicker/src/App.js
@@ -1,39 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import cookie from './cookie.png';
+import grandma from './grandma.jpg';
 
 function App(){
   const [dessert, setDessert] = useState(0);
   const [dessertMultiplier, setDessertMultiplier] = useState(1);
-  const [cost, setCost] = useState(10);
+  const [costClickMultiplier, setCostClickMultiplier] = useState(10);
+  const [costGrandmaUpgrade, setCostGrandmaUpgrade] = useState(100);
+  const [autoClick, setAutoClick] = useState(1);
 
   const handleDessertClicks = () => {
     setDessert (dessert + dessertMultiplier);
   }
 
   const buyMultiplier = () => {
-    if (dessert >= cost){
-      setDessert(dessert-cost);
+    if (dessert >= costClickMultiplier){
+      setDessert(dessert-costClickMultiplier);
       setDessertMultiplier(dessertMultiplier + 1);
     }
     else{
       alert("Not enough desserts to buy the multiplier!")
     }
-    setCost(Math.pow(10, dessertMultiplier+1));
+    setCostClickMultiplier(Math.pow(10, dessertMultiplier+1));
   };
+
+  const buyGrandmaUpgrade = () =>{
+    if(dessert >= costGrandmaUpgrade){
+      setDessert(dessert-costGrandmaUpgrade);
+      setAutoClick(autoClick + 1);
+      setCostGrandmaUpgrade(costGrandmaUpgrade*10);
+    }
+    else{
+      alert("Not enough desserts to buy the grandma! Get yo cookies up, not yo funny!");
+    }
+  };
+
+  useEffect(() => {
+    let interval;
+    if(autoClick > 1) {
+      interval = setInterval(() => {
+        setDessert((prevDessert) => prevDessert + autoClick - 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [autoClick]);
+
+
 return(
   <div className='App'>
     <h1 className='Title'>Dessert Clicker!</h1>
     <div>
-    <button onClick={handleDessertClicks}><img src='cookie.png' alt = 'Cookie' className='Cookie'/></button>
+    <button onClick={handleDessertClicks}><img src={cookie} alt = 'Cookie' className='Cookie' style={{width:'500px', height:'500px'}}/></button>
     </div>
     <div>
     <text>Your Desserts: {dessert}</text>
     </div>
     <div>
-      <button onClick={buyMultiplier}>Cost: {cost}</button>
+      <button onClick={buyMultiplier}>Cost: {costClickMultiplier}</button>
     </div>
     <div>
       <text>Current Multiplier: {dessertMultiplier}</text>
+    </div>
+    <div>
+      <button onClick={buyGrandmaUpgrade}><img src={grandma} alt= 'Grandma' style ={{width:'200px', height: '200px'}}/></button>
+    </div>
+    <div>
+      Cost: {costGrandmaUpgrade}
+    </div>
+    <div>
+      {autoClick > 0 &&(
+        <text>Grandma is making : +{autoClick-1} cookies/sec</text>
+      )}
     </div>
   </div>
 )
