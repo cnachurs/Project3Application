f2bf0a1b68d6b756266f0e42753a392d914dabb3
cnachurs <cnachurs@villanova.edu>
2023-12-06 15:24:33 -0500

    Added new App.js and the clicker game is working

--- dessert_clicker/src/App.js
+++ dessert_clicker/src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Title from './Title';
-import Cookie from './Cookie';
 
-const App = () =>{
-  return(
-    <div className='App'>
-      <Title text ="Dessert Clicker!"/>
-      <Cookie/>
+function App(){
+  const [dessert, setDessert] = useState(0);
+  const [dessertMultiplier, setDessertMultiplier] = useState(1);
+  const [cost, setCost] = useState(10);
+
+  const handleDessertClicks = () => {
+    setDessert (dessert + dessertMultiplier);
+  }
+
+  const buyMultiplier = () => {
+    if (dessert >= cost){
+      setDessert(dessert-cost);
+      setDessertMultiplier(dessertMultiplier + 1);
+    }
+    else{
+      alert("Not enough desserts to buy the multiplier!")
+    }
+    setCost(Math.pow(10, dessertMultiplier));
+  };
+return(
+  <div className='App'>
+    <h1 className='Title'>Dessert Clicker!</h1>
+    <div>
+    <button onClick={handleDessertClicks}><img src='cookie.png' alt = 'Cookie' className='Cookie'/></button>
     </div>
-  );
-};
+    <div>
+    <text>Your Desserts: {dessert}</text>
+    </div>
+    <div>
+      <button onClick={buyMultiplier}>Cost: {cost}</button>
+    </div>
+    <div>
+      <text>Current Multiplier: {dessertMultiplier}</text>
+    </div>
+  </div>
+)
+}
+
 export default App;
